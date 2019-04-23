70cbce587b4afd2adc9f3bfbda7a7deb2c3bf4f9
Győry Enikő <gyory.eniko@gmail.com>
2019-04-23 20:42:51 +0200

    valami

--- beadando_3.py
+++ beadando_3.py
@@ -1,1 +1,12 @@
-adottszam=int(input)
+try:
+    adottszam=int(input("Adjon meg egy egész számot!"))
+    ls = []
+    for i in range(100):
+        szam = str(i)
+        forditva = szam[::-1]
+        if szam == forditva:
+            print(szam)
+
+
+except ValueError:
+    print("Nem egész számot adott meg!")
