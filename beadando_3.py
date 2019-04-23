164bee07eb2aae3c5cb3a797eca9359a2122acc7
Győry Enikő <gyory.eniko@gmail.com>
2019-04-23 20:48:24 +0200

    valami

--- beadando_3.py
+++ beadando_3.py
@@ -1,10 +1,10 @@
 try:
     adottszam=int(input("Adjon meg egy egész számot!"))
     ls = []
-    for i in range(100):
+    for i in range(100001):
         szam = str(i)
         forditva = szam[::-1]
-        if szam == forditva:
+        if szam == forditva and i % adottszam == 0:
             print(szam)
 
 
