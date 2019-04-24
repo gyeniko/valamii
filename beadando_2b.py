a59bfa366cd6c41d20516dfd689acd7f1390adc0
Győry Enikő <gyory.eniko@gmail.com>
2019-04-24 19:35:49 +0200

    valami

--- beadando_2b.py
+++ beadando_2b.py
@@ -9,6 +9,9 @@ try:
     ls = [1]
     for i in range(n1):
         print(ls)
+        if i+1 == n2:
+            plt.plot(ls,"go")
+            plt.show()
         ls.append(0)
         for j in range(len(ls)):
             if j == 0:
@@ -16,9 +19,7 @@ try:
             else:
                 ls2.append(ls[j-1]+ls[j])
         ls = ls2
-        if i == n2:
-            plt.plot(ls,"go")
-            plt.show()
+
 
 except ValueError:
     print("Nem egész számokat adott meg")
