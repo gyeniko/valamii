efd10fd494d60c4f6016243e013e707b17f252a4
Győry Enikő <gyory.eniko@gmail.com>
2019-04-24 19:25:44 +0200

    valami

--- beadando_2b.py
+++ beadando_2b.py
@@ -1,11 +1,24 @@
-n1 = 5
-ls = [1]
-for i in range(n1):
-    print(ls)
-    ls.append(0)
-    for j in range(len(ls)):
-        if j == 0:
-            ls2=[1]
-        else:
-            ls2.append(ls[j-1]+ls[j])
-    ls = ls2
+import matplotlib.pyplot as plt
+
+try:
+    n1 = int(input("Add meg az első számot! "))
+    n2 = int(input("Add meg a második számot! "))
+
+    #ide kell majd egy ellenőrzés!!!
+
+    ls = [1]
+    for i in range(n1):
+        print(ls)
+        ls.append(0)
+        for j in range(len(ls)):
+            if j == 0:
+                ls2=[1]
+            else:
+                ls2.append(ls[j-1]+ls[j])
+        ls = ls2
+        if i == n2:
+            plt.plot(ls,"go")
+            plt.show()
+
+except ValueError:
+    print("Nem egész számokat adott meg")
