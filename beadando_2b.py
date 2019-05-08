05a3e6fc0c31f2e24d9bfa09467403ba44a03b2a
Győry Enikő <gyory.eniko@gmail.com>
2019-05-08 22:07:23 +0200

    valami

--- beadando_2b.py
+++ beadando_2b.py
@@ -4,22 +4,23 @@ try:
     n1 = int(input("Add meg az első számot! "))
     n2 = int(input("Add meg a második számot! "))
 
-    #ide kell majd egy ellenőrzés!!!
-
-    ls = [1]
-    for i in range(n1):
-        print(ls)
-        if i+1 == n2:
-            plt.plot(ls,"go")
-            plt.show()
-        ls.append(0)
-        for j in range(len(ls)):
-            if j == 0:
-                ls2=[1]
-            else:
-                ls2.append(ls[j-1]+ls[j])
-        ls = ls2
-
+    def pascalhsz(n1,n2):
+        if n1 <= n2:
+            return "Az n1 nem nagyobb, mint az n2"
+        ls = [1]
+        for i in range(n1):
+            print(ls)
+            if i+1 == n2:
+                plt.plot(ls,"go")
+                plt.show()
+            ls.append(0)
+            for j in range(len(ls)):
+                if j == 0:
+                    ls2=[1]
+                else:
+                    ls2.append(ls[j-1]+ls[j])
+            ls = ls2
+    pascalhsz(n1,n2)
 
 except ValueError:
     print("Nem egész számokat adott meg")
