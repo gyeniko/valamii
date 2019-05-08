12237ff03730c94b2670d4b55c6fd1ca87a7012a
Győry Enikő <gyory.eniko@gmail.com>
2019-05-08 22:10:22 +0200

    valami

--- beadando_2b.py
+++ beadando_2b.py
@@ -12,7 +12,7 @@ try:
             print(ls)
             if i+1 == n2:
                 plt.plot(ls,"go")
-                plt.show()
+
             ls.append(0)
             for j in range(len(ls)):
                 if j == 0:
@@ -20,6 +20,7 @@ try:
                 else:
                     ls2.append(ls[j-1]+ls[j])
             ls = ls2
+        plt.show()
     pascalhsz(n1,n2)
 
 except ValueError:
